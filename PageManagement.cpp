77fb731cc41b0b50d89d7b5802cce3ac7dd5bbda
Esteban Hernandez <69884185+EstebanHernandezz@users.noreply.github.com>
2024-04-02 15:22:40 -0500

    Update PageManagement.cpp

--- PageManagement.cpp
+++ PageManagement.cpp
@@ -4,7 +4,7 @@
 std::pair<unsigned int, unsigned int> PageManagement::extractPageNumberAndOffset(unsigned int logicalAddress) {
     // calculate the page number from logical address, keep only the last 8 bits
     unsigned int pageNumber = (logicalAddress >> 8) & 0xFF;
-    // calculate the offset -> ...
+    // calculate the offset -> ...keep LOWER 8 bits NOT upper
     unsigned int offset = logicalAddress & 0xFF;
 
     return { pageNumber, offset };
